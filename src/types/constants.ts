a079da0bb6d554e0047b36e8e73fada62799f551
rangggu <aliyah521@naver.com>
2024-11-26 21:52:40 +0900

    Comment : 셀 상태에 대한 선언 주석 변경

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -6,12 +6,12 @@ export const enum STATUS {
 }
 
 export const enum CELL_STATE {
-  EMPTY = 0, // 숨겨진 상태 (사용자가 아직 클릭하지 않은 상태)
-  NUMBER = 1, // 숫자가 적혀 있는 상태 (주변 지뢰의 개수)
-  MINE = 2, // 지뢰가 있는 상태
-  FLAGGED_MINE = 3, // 지뢰가 있는 칸에 깃발이 꽂혀 있는 상태
-  FLAGGED_NON_MINE = 4, // 지뢰가 아닌 곳에 깃발이 꽂혀 있는 상태
-  OPENED_MINE = 5, // 지뢰가 있는 칸이 열려서 게임 오버 상태
-  OPENED_NUMBER = 6, // 숫자가 적혀 있는 칸이 열린 상태
-  OPENED_EMPTY = 7, // 빈 칸이 열린 상태 (주변에 지뢰가 없어서 열렸을 때)
+  EMPTY = 0, // 빈 칸
+  NUMBER = 1, // 숫자
+  MINE = 2, // 지뢰
+  FLAGGED_MINE = 3, // 지뢰인 곳에 깃발
+  FLAGGED_NON_MINE = 4, // 지뢰가 아닌 곳에 깃발
+  OPENED_MINE = 5, // 열린 지뢰
+  OPENED_NUMBER = 6, // 열린 숫자
+  OPENED_EMPTY = 7, // 열린 빈 칸
 }
