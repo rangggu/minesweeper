34805e41480931587e154873f8ab6a85a4c641fb
rangggu <aliyah521@naver.com>
2024-11-26 21:38:02 +0900

    Feat : 셀 클릭 이벤트 함수 구현
    - 좌/우 셀 클릭 이벤트 함수

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import reducer from "./reducer/reducer"
 
-const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem("control")
-    if (serializedState) {
-      return { control: JSON.parse(serializedState) }
-    }
-  } catch (err) {
-    console.error(err)
-  }
-  return undefined
-}
-
-const initialState = loadState()
-
 const store = configureStore({
   reducer,
-  preloadedState: initialState,
-})
-
-store.subscribe(() => {
-  const state = store.getState()
-  try {
-    const serializedState = JSON.stringify(state.control)
-    localStorage.setItem("control", serializedState)
-  } catch (err) {
-    console.error(err)
-  }
 })
 
 export type RootState = ReturnType<typeof store.getState>
--- src/store/slices/control/index.ts
+++ src/store/slices/control/index.ts
@@ -1,13 +1,109 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { CELL_STATE, STATUS } from "../../../types/constants"
+import { initialArray, openCells } from "../../../utils/control"
 
-const initialState = {}
+export interface ControlState {
+  board: number[][]
+  mines: number[][]
+  width: number
+  height: number
+  status: STATUS
+  flagCount: number
+  mineCount: number
+}
 
-export const surveySlice = createSlice({
+interface CellPosition {
+  row: number
+  col: number
+}
+
+const { board, mines } = initialArray(8, 16, 40)
+
+const initialState = {
+  board: board,
+  mines: mines,
+  width: 8,
+  height: 16,
+  status: STATUS.READY,
+  flagCount: 0,
+  mineCount: 40,
+}
+
+export const controlSlice = createSlice({
   name: "control",
   initialState,
-  reducers: {},
+  reducers: {
+    leftClickCell(state, action: PayloadAction<CellPosition>) {
+      const { row, col } = action.payload
+      const cellState = state.board[row][col]
+
+      // 게임 상태가 READY 이고 첫 클릭 셀에 지뢰가 있으면
+      if (state.status === STATUS.READY) {
+        if (cellState === CELL_STATE.MINE) {
+          const { board, mines } = initialArray(state.width, state.height, state.mineCount, row, col)
+          // 지뢰 재배치
+          state.board = board
+          state.mines = mines
+        }
+      }
+
+      if (
+        cellState === CELL_STATE.OPENED_NUMBER ||
+        cellState === CELL_STATE.OPENED_EMPTY ||
+        cellState === CELL_STATE.OPENED_MINE
+      ) {
+        return
+      }
+
+      // 지뢰 클릭
+      if (cellState === CELL_STATE.MINE) {
+        state.board[row][col] = CELL_STATE.OPENED_MINE
+        state.status = STATUS.GAMEOVER
+      }
+      // 숫자 클릭
+      else if (cellState === CELL_STATE.NUMBER) {
+        state.board[row][col] = CELL_STATE.OPENED_NUMBER
+      }
+      // 빈 칸 클릭
+      else if (cellState === CELL_STATE.EMPTY) {
+        state.board = openCells(row, col, state.board, state.mines)
+        state.board[row][col] = CELL_STATE.OPENED_EMPTY
+      }
+
+      state.status = STATUS.RUN // 게임 상태를 RUN으로 변경
+    },
+
+    rightClickCell(state, action: PayloadAction<CellPosition>) {
+      const { row, col } = action.payload
+      const cellState = state.board[row][col]
+
+      if (state.status === STATUS.GAMEOVER || state.status === STATUS.SUCCESS) return
+      if (
+        cellState === CELL_STATE.OPENED_EMPTY ||
+        cellState === CELL_STATE.OPENED_NUMBER ||
+        cellState === CELL_STATE.OPENED_MINE
+      ) {
+        return
+      }
+
+      // 깃발 꽂기 (지뢰 o)
+      if (cellState === CELL_STATE.MINE) {
+        console.log("isMine")
+        state.board[row][col] = CELL_STATE.FLAGGED_MINE
+      }
+      // 깃발 꽂기 (지뢰 x)
+      else if (cellState === CELL_STATE.NUMBER || cellState === CELL_STATE.EMPTY) {
+        console.log("not Mine")
+        state.board[row][col] = CELL_STATE.FLAGGED_NON_MINE
+      }
+      // 깃발 제거
+      else if (cellState === CELL_STATE.FLAGGED_MINE || cellState === CELL_STATE.FLAGGED_NON_MINE) {
+        state.board[row][col] = CELL_STATE.EMPTY
+      }
+    },
+  },
 })
 
-export const {} = surveySlice.actions
+export const { leftClickCell, rightClickCell } = controlSlice.actions
 
-export default surveySlice.reducer
+export default controlSlice.reducer
