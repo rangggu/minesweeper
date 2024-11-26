a19c6393d0d33ba7989a71d4a41976f314eb9283
rangggu <aliyah521@naver.com>
2024-11-26 21:32:41 +0900

    Feat : 셀 및 게임의 상태 선언

---
+++ src/types/constants.ts
@@ -0,0 +1,17 @@
+export const enum STATUS {
+  READY = "ready",
+  RUN = "run",
+  SUCCESS = "success",
+  GAMEOVER = "gameover",
+}
+
+export const enum CELL_STATE {
+  HIDDEN = 0, // 숨겨진 상태 (사용자가 아직 클릭하지 않은 상태)
+  NUMBER = 1, // 숫자가 적혀 있는 상태 (주변 지뢰의 개수)
+  MINE = 2, // 지뢰가 있는 상태
+  FLAGGED_MINE = 3, // 지뢰가 있는 칸에 깃발이 꽂혀 있는 상태
+  FLAGGED_NON_MINE = 4, // 지뢰가 아닌 곳에 깃발이 꽂혀 있는 상태
+  OPENED_MINE = 5, // 지뢰가 있는 칸이 열려서 게임 오버 상태
+  OPENED_NUMBER = 6, // 숫자가 적혀 있는 칸이 열린 상태
+  OPENED_EMPTY = 7, // 빈 칸이 열린 상태 (주변에 지뢰가 없어서 열렸을 때)
+}
