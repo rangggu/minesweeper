0fb0ed665f756573b1df595aef63a46f394c9be9
rangggu <aliyah521@naver.com>
2024-11-26 21:36:09 +0900

    Feat : 유틸 함수 구현
    - 배열 생성 함수
    - 2D 배열 생성 함수
    - 한 셀의 크기 계산 함수

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
+
+// @Note : 배열 생성 함수
+export const createArray = (length: number, value: any) => {
+  return Array(length).fill(value)
+}
+
+// @Note : 2D 배열 생성 함수
+export const createArray2D = (width: number, height: number, initialValue: any): any[][] => {
+  const array: any[][] = []
+  for (let i = 0; i < height; i++) {
+    array.push(new Array(width).fill(initialValue))
+  }
+  return array
+}
+
+// @Note : 한 셀의 크기 계산 함수
+export const calculateCellSize = (
+  containerWidth: number,
+  containerHeight: number,
+  row: number,
+  col: number,
+): number => {
+  const maxCellWidth = containerWidth / col
+  const maxCellHeight = containerHeight / row
+
+  return Math.floor(Math.min(maxCellWidth, maxCellHeight))
+}
